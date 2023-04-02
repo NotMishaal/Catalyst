a9af7896b592667e4d2f741bbba3fa5fc0441d72
Mishaal <mishaal.20210822@iit.ac.lk>
2023-04-02 14:23:09 +0530

    matched style of 1st part

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,24 +4,24 @@ function Home() {
   return (
     <div className="Home">
       <NavBar />
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col">
-            <h1 className="fw-bold">
+      <div className="container-fluid ml-5 ">
+        <div className="row align-items-center vh-100 vw-100">
+          <div className="col mx-5">
+            <h1 className="fw-bold display-1">
               Welcome to Catalyst, your all-in-one platform for startup success.
             </h1>
-            <p>
+            <p className="fw-medium">
               We believe that every startup has the potential to succeed with
               the right resources and support. That's why we offer a wide range
               of services to help startups at every stage of their journey, from
               ideation to growth.
             </p>
-            <button type="button" className="btn btn-primary">
+            <button type="button" className="btn btn-primary btn-lg py-2 px-5">
               Start Now
             </button>
           </div>
           <div className="col">
-            <img src="../../public/sheet.svg"></img>
+            <img src="../../public/sheet.svg" className="img-fluid w-75" />
           </div>
         </div>
       </div>

e72de2d819334fc4502159bdc08880dae05a0895
Mishaal <mishaal.20210822@iit.ac.lk>
2023-04-15 16:04:40 +0530

    added base code for routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,18 @@ import "./App.css";
 import { Auth } from "./components/Auth";
 import NavBar from "./components/Nav";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+    // <div className="App">
+    //   <Home />
+    // </div>
   );
 }
 
